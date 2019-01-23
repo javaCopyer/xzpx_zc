feefa07c3a7a6778e5835f87718314b46f6bfec2
sky <zxc@domain123.com>
2019-01-23 14:29:25 +0800

    123

--- xzpx_zc/src/com/xzpx_zc/util/SocketUtil.java
+++ xzpx_zc/src/com/xzpx_zc/util/SocketUtil.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.http.HttpEntity;   
-import org.apache.http.NameValuePair;  //**************  不要导入错jar包  不是httpClient
+import org.apache.http.NameValuePair;  //**************  涓嶈瀵煎叆閿檍ar鍖�  涓嶆槸httpClient
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -30,7 +30,7 @@ import org.apache.http.util.EntityUtils;
 
 /**
  * httpClient
- * 访问网络资源
+ * 璁块棶缃戠粶璧勬簮
  * @author zhangchao
  *
  */
@@ -44,16 +44,16 @@ public class SocketUtil {
 //			Type MAP_TYPE = new TypeToken<Map<String, Object>>() {}.getType();
 //			Gson gson = new GsonBuilder().serializeNulls().create();
 //			postJSON("http://127.0.0.1/estore/adminLogin.action", gson.toJson(map, MAP_TYPE));
-			String s = 
-						"[{\"userName\": \"张超1\", \"mobile\":\"15062259016\", \"products\":[{\"name\": \"哇哈哈\"}]},"
-						+ "{\"userName\": \"张超2\", \"mobile\":\"15062259016\",\"products\":[{\"name\": \"怕怕烫\"}]}]";
-			//s = "<?xml version=\"1.0\" standalone=\"yes\"?><product id=\"2\"><area>徐州</area><name>疯狂Java讲义</name></product>";  
-			//String o = postJSON("http://127.0.0.1:8080/springDemo/select1.action", s);
-			//	String result = new String(o.getBytes("ISO8859-1"), "UTF-8");
-			Map<String, String> map = new HashMap<String, String>();
-			map.put("products[1].name", "始于初见，止于终老");
-			String o = postRequest("http://127.0.0.1:8080/springDemo/select1.action", map);
-			System.out.println(o);
+//			String s = 
+//						"[{\"userName\": \"寮犺秴1\", \"mobile\":\"15062259016\", \"products\":[{\"name\": \"鍝囧搱鍝圽"}]},"
+//						+ "{\"userName\": \"寮犺秴2\", \"mobile\":\"15062259016\",\"products\":[{\"name\": \"鎬曟�曠儷\"}]}]";
+//			//s = "<?xml version=\"1.0\" standalone=\"yes\"?><product id=\"2\"><area>寰愬窞</area><name>鐤媯Java璁蹭箟</name></product>";  
+//			//String o = postJSON("http://127.0.0.1:8080/springDemo/select1.action", s);
+//			//	String result = new String(o.getBytes("ISO8859-1"), "UTF-8");
+//			Map<String, String> map = new HashMap<String, String>();
+//			map.put("products[1].name", "濮嬩簬鍒濊锛屾浜庣粓鑰�");
+//			String o = postRequest("http://127.0.0.1:8080/springDemo/select1.action", map);
+//			System.out.println(o);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -74,7 +74,7 @@ public class SocketUtil {
 	  try {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpPost httpPost = new HttpPost(url);
-		//创建参数队列
+		//鍒涘缓鍙傛暟闃熷垪
 		List<NameValuePair> formParams = new ArrayList<NameValuePair>();
 		Iterator<String> iterator =  map.keySet().iterator();
 		while(iterator.hasNext()) {
@@ -95,7 +95,7 @@ public class SocketUtil {
 		return null;
 	}
 	/**
-	 * 指定参数名发送xml
+	 * 鎸囧畾鍙傛暟鍚嶅彂閫亁ml
 	 * @param url
 	 * @param xml
 	 * @return
@@ -117,7 +117,7 @@ public class SocketUtil {
 		return null;
 	}
 	/**
-	 * 不指定参数发送xml
+	 * 涓嶆寚瀹氬弬鏁板彂閫亁ml
 	 */
 	public static String postXML(String url, String xml) throws Exception{
 		CloseableHttpClient httpClient = HttpClients.createDefault();
@@ -135,7 +135,7 @@ public class SocketUtil {
 		return null;
 	}
 	/**
-	 * 不指定参数发送json
+	 * 涓嶆寚瀹氬弬鏁板彂閫乯son
 	 */
 	public static String postJSON(String url, String json) throws Exception {
 		CloseableHttpClient httpClient = HttpClients.createDefault();
@@ -148,14 +148,14 @@ public class SocketUtil {
          if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
  			HttpEntity entity = response.getEntity();
  			//System.out.println(EntityUtils.toString(entity).trim());
- 			//EntityUtils.toString方法在调用的方法中只能被使用一下，反之则抛出IOException
+ 			//EntityUtils.toString鏂规硶鍦ㄨ皟鐢ㄧ殑鏂规硶涓彧鑳借浣跨敤涓�涓嬶紝鍙嶄箣鍒欐姏鍑篒OException
  			return EntityUtils.toString(entity).trim();
  		}
          
 		return null;
 	}
 	/**
-	 * 上传文件
+	 * 涓婁紶鏂囦欢
 	 */
 }
 
